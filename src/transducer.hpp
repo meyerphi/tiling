5a1dfcb776ee0407b8a8696e0dd2430af103aa68
Philipp Meyer <meyerphi@in.tum.de>
2019-11-04 19:14:42 +0100

    First step to optimize memory consumption by transducer

--- src/transducer.hpp
+++ src/transducer.hpp
@@ -7,13 +7,16 @@
 
 #include "tiles.hpp"
 
-typedef int Index;
+typedef unsigned int Index;
+constexpr Index unexplored = std::numeric_limits<Index>::max();
 
 struct Edge {
     Index target;
     Color north;
     Color south;
     std::vector<TileIndex> tiles;
+
+    Edge() { assert(false); }
 
     Edge(Index target, Color north, Color south) :
         target(target), north(north), south(south)
@@ -34,7 +37,7 @@ struct Edge {
 // optimized stack frame for Tarjan and DFS
 struct StackFrame {
     Index node;
-    size_t edge;
+    Index edge;
 };
 
 /**
@@ -95,9 +98,16 @@ struct DoubleStack {
 	}
 };
 
+struct EdgeRange {
+    Index begin;
+    Index end;
+};
+
 struct Transducer {
-    size_t num_nodes;
+    Index num_nodes;
     std::vector<std::vector<Edge>> succ_edges;
+    std::vector<EdgeRange> nodes;
+    std::vector<Edge> edges;
 
     /*
      * Create empty transducer
@@ -110,11 +120,11 @@ struct Transducer {
      * Create identity transducer for given number of colors
      */
     Transducer(const Color colors) :
-        num_nodes(1)
+        num_nodes(0)
     {
-        succ_edges.push_back({});
+        Index i = add_node();
         for (Color c = 0; c < colors; c++) {
-            add_edge(0, 0, c, c);
+            add_edge(i, i, c, c);
         }
     }
 
@@ -122,38 +132,73 @@ struct Transducer {
      * Create transducer for given number of color and set of tiles.
      */
     Transducer(const Color num_colors, const std::vector<Tile>& tiles) :
-        num_nodes(num_colors)
+        num_nodes(0)
     {
-        succ_edges.resize(num_nodes);
+        // group tiles by source color
+        std::vector<std::vector<std::pair<Tile, TileIndex>>> source_tiles(num_colors);
+
         TileIndex t = 0;
-        for (auto it = std::cbegin(tiles); it < std::cend(tiles); it++, t++) {
-            const Tile& tile = *it;
-            Edge* edge = add_edge(tile.west, tile.east, tile.north, tile.south);
-            if (edge != nullptr) {
-                edge->tiles.push_back(t);
+        for (const Tile& tile : tiles) {
+            source_tiles[tile.west].push_back({tile, t});
+            t++;
+        }
+
+        for (const auto& tileset : source_tiles) {
+            Index i = add_node();
+            for (const auto& entry : tileset) {
+                const Tile& tile = entry.first;
+                const TileIndex t = entry.second;
+                assert(i == tile.west);
+                Edge& edge = add_edge(tile.west, tile.east, tile.north, tile.south);
+                edge.tiles.push_back(t);
             }
         }
     }
 
     Index add_node() {
+        const Index current_end = (Index)edges.size();
+        nodes.push_back({current_end, current_end});
+
+        /*
+        if (num_nodes > 0) {
+            assert(nodes[num_nodes-1].end == current_end);
+        }
+        */
         Index i = num_nodes;
-        succ_edges.push_back({});
         num_nodes++;
         return i;
     }
 
-    Edge* add_edge(Index source, Index target, Color north, Color south) {
-        Edge edge(target, north, south);
-        // do not add duplicate edges
-        std::vector<Edge>& source_edges = succ_edges[source];
-        const auto find_it = std::find(std::begin(source_edges), std::end(source_edges), edge);
-        if (find_it == std::end(source_edges)) {
-            succ_edges[source].push_back(std::move(edge));
-            return &succ_edges[source].back();
+    Index get_edge_begin(Index v) const {
+        return nodes[v].begin;
+    }
+    Index get_edge_end(Index v) const {
+        return nodes[v].end;
+    }
+    const Edge& get_edge(Index e) const {
+        assert(e >= 0);
+        assert(e < edges.size());
+        return edges[e];
+    }
+
+    void update_node(Index i) {
+        const Index current_end = (Index)edges.size();
+        nodes[i].begin = current_end;
+        nodes[i].end = current_end;
+    }
+
+    Edge& add_edge(Index source, Index target, Color north, Color south) {
+        if(nodes[source].begin == nodes[source].end) {
+            update_node(source);
         }
-        else {
-            return nullptr;
+        assert(nodes[source].end == edges.size());
+        if (source > 0) {
+            assert(nodes[source-1].end == nodes[source].begin);
         }
+
+        nodes[source].end++;
+        edges.emplace_back(target, north, south);
+        return edges.back();
     }
 
     inline void pearce_begin_visiting(
@@ -163,7 +208,7 @@ struct Transducer {
             Index& index,
             Index v
     ) const {
-        stack.push_front({v, 0});
+        stack.push_front({v, get_edge_begin(v)});
         root[v] = true;
         rindex[v] = index;
         index++;
@@ -178,11 +223,12 @@ struct Transducer {
     ) const {
         StackFrame& frame = stack.top_front();
         const Index v = frame.node;
-        const size_t end = succ_edges[v].size();
-        for (size_t i = frame.edge; i <= end; i++) {
-            if (i > 0) {
+        const Index begin = get_edge_begin(v);
+        const Index end = get_edge_end(v);
+        for (Index i = frame.edge; i <= end; i++) {
+            if (i > begin) {
                 // finish edge
-                const Index w = succ_edges[v][i-1].target;
+                const Index w = get_edge(i-1).target;
                 if (rindex[w] < rindex[v]) {
                     rindex[v] = rindex[w];
                     root[v] = false;
@@ -190,7 +236,7 @@ struct Transducer {
             }
             if (i < end) {
                 // begin edge
-                const Index w = succ_edges[v][i].target;
+                const Index w = get_edge(i).target;
                 if (rindex[w] == 0) {
                     frame.edge = i+1;
                     pearce_begin_visiting(rindex, root, stack, index, w);
@@ -228,7 +274,7 @@ struct Transducer {
         Index index = 1;
         Index c = num_nodes - 1;
 
-        for (size_t v = 0; v < num_nodes; v++) {
+        for (Index v = 0; v < num_nodes; v++) {
             if (rindex[v] == 0) {
                 pearce_begin_visiting(rindex, root, stack, index, v);
                 while (!stack.empty_front()) {
@@ -249,17 +295,25 @@ struct Transducer {
     void simplify() {
         std::vector<Index> scc_ids = compute_sccs();
 
-        for (size_t i = 0; i < num_nodes; i++) {
+        Index cur_start = 0;
+        Index cur_index = 0;
+        std::vector<Edge> new_edges;
+
+        for (Index i = 0; i < num_nodes; i++) {
             const Index source_scc_id = scc_ids[i];
-            std::vector<Edge> new_succ_edges;
-            for (size_t j = 0; j < succ_edges[i].size(); j++) {
-                Edge& edge = succ_edges[i][j];
+            for (Index j = get_edge_begin(i); j < get_edge_end(i); j++) {
+                Edge& edge = edges[j];
                 if (source_scc_id == scc_ids[edge.target]) {
-                    new_succ_edges.push_back(std::move(edge));
+                    new_edges.push_back(std::move(edge));
+                    cur_index++;
                 }
             }
-            succ_edges[i].swap(new_succ_edges);
+            nodes[i].begin = cur_start;
+            nodes[i].end = cur_index;
+            cur_start = cur_index;
         }
+
+        edges.swap(new_edges);
     }
 
     inline void cycle_begin_visiting(
@@ -268,7 +322,7 @@ struct Transducer {
             std::stack<StackFrame, std::vector<StackFrame>>& stack,
             Index v
     ) const {
-        stack.push({v, 0});
+        stack.push({v, get_edge_begin(v)});
         visited[v] = true;
         on_stack[v] = true;
     }
@@ -282,10 +336,10 @@ struct Transducer {
     ) const {
         StackFrame& frame = stack.top();
         const Index v = frame.node;
-        const size_t end = succ_edges[v].size();
-        for (size_t i = frame.edge; i < end; i++) {
+        const Index end = get_edge_end(v);
+        for (Index i = frame.edge; i < end; i++) {
             // begin edge
-            const Edge& edge = succ_edges[v][i];
+            const Edge& edge = get_edge(i);
             if (!periodic || edge.north == edge.south) {
                 const Index w = edge.target;
                 if (!visited[w]) {
@@ -296,11 +350,11 @@ struct Transducer {
                 }
                 else if (on_stack[w]) {
                     // found cycle
-                    cycle.push_back(succ_edges[v][i]);
+                    cycle.push_back(get_edge(i));
                     while (stack.top().node != w) {
                         stack.pop();
                         frame = stack.top();
-                        cycle.push_back(succ_edges[frame.node][frame.edge-1]);
+                        cycle.push_back(get_edge(frame.edge-1));
                     }
                     std::reverse(std::begin(cycle), std::end(cycle));
                     return true;
@@ -313,7 +367,21 @@ struct Transducer {
         return false;
     }
 
+    void print_debug() {
+        std::cout << "Node array:" << std::endl;
+        for (EdgeRange i : nodes) {
+            std::cout << " [" << i.begin << "," << i.end << "]";
+        }
+        std::cout << std::endl;
+        std::cout << "Edge array:" << std::endl;
+        for (const Edge& e : edges) {
+            std::cout << " " << e.target;
+        }
+        std::cout << std::endl;
+    }
+
     std::vector<Edge> find_cycle(const bool periodic) const {
+
         std::vector<bool> visited(num_nodes, false);
         std::vector<bool> on_stack(num_nodes, false);
 
@@ -322,7 +390,7 @@ struct Transducer {
 
         std::vector<Edge> cycle;
 
-        for (size_t v = 0; v < num_nodes; v++) {
+        for (Index v = 0; v < num_nodes; v++) {
             if (!visited[v]) {
                 cycle_begin_visiting(visited, on_stack, stack, v);
                 while (!stack.empty()) {
@@ -363,8 +431,8 @@ struct Transducer {
     }
 
     bool empty() const {
-        for (size_t i = 0; i < num_nodes; i++) {
-            if (!succ_edges[i].empty()) {
+        for (Index i = 0; i < num_nodes; i++) {
+            if (get_edge_begin(i) != get_edge_end(i)) {
                 return false;
             }
         }
@@ -376,44 +444,82 @@ struct Transducer {
      * and return the result.
      */
     Transducer compose(const Transducer& trans2) const {
-        size_t product_size = num_nodes * trans2.num_nodes;
+        const Index product_size = num_nodes * trans2.num_nodes;
         // detect overflow
         assert (product_size / num_nodes == trans2.num_nodes);
 
         Transducer composition;
-        std::vector<Index> node_map(product_size, -1);
+        std::vector<Index> node_map(product_size, unexplored);
 
-        for (size_t i1 = 0; i1 < num_nodes; i1++) {
-            for (const Edge& edge1 : succ_edges[i1]) {
-                for (size_t i2 = 0; i2 < trans2.num_nodes; i2++) {
+        std::vector<bool> visited(product_size, false);
+        std::deque<std::pair<Index, Index>> queue;
+
+        for (Index i1 = 0; i1 < num_nodes; i1++) {
+            for (Index i2 = 0; i2 < trans2.num_nodes; i2++) {
+                const Index i12 = i1 + i2*num_nodes;
+
+                if (!visited[i12]) {
+                    visited[i12] = true;
+                    queue.push_back({i1, i2});
+                    //std::cout << "Adding to queue unknown node from " << i1 << "," << i2 << std::endl; 
+                }
+
+                while(!queue.empty()) {
+                    const auto entry = queue.front();
+                    queue.pop_front();
+                    const Index i1 = entry.first;
+                    const Index i2 = entry.second;
                     const Index i12 = i1 + i2*num_nodes;
-                    for (const Edge& edge2 : trans2.succ_edges[i2]) {
-                        if (edge1.south == edge2.north) {
-                            Index j1 = edge1.target;
-                            Index j2 = edge2.target;
-                            const Index j12 = j1 + j2*num_nodes;
+                    //std::cout << "Popping from queue unknown node from " << i1 << "," << i2 << std::endl; 
 
-                            // add/find source node
-                            Index pred_index = node_map[i12];
-                            if (pred_index == -1) {
-                                pred_index = composition.add_node();
-                                node_map[i12] = pred_index;
-                            }
+                    Index pred_index = node_map[i12];
+                    if (pred_index != unexplored) {
+                        composition.update_node(pred_index);
+                    }
 
-                            // add/find target node
-                            Index succ_index = node_map[j12];
-                            if (succ_index == -1) {
-                                succ_index = composition.add_node();
-                                node_map[j12] = succ_index;
-                            }
+                    for (Index k1 = get_edge_begin(i1); k1 < get_edge_end(i1); k1++) {
+                        const Edge& edge1 = get_edge(k1);
+                        for (Index k2 = trans2.get_edge_begin(i2); k2 < trans2.get_edge_end(i2); k2++) {
+                            const Edge& edge2 = trans2.get_edge(k2);
+                            if (edge1.south == edge2.north) {
+                                Index j1 = edge1.target;
+                                Index j2 = edge2.target;
 
-                            Edge* edge = composition.add_edge(pred_index, succ_index, edge1.north, edge2.south);
-                            if (edge != nullptr) {
+                                const Index j12 = j1 + j2*num_nodes;
+
+                                // add/find source node
+                                Index pred_index = node_map[i12];
+                                if (pred_index == unexplored) {
+                                    pred_index = composition.add_node();
+                                    node_map[i12] = pred_index;
+                                }
+
+                                // add/find target node
+                                Index succ_index = node_map[j12];
+                                if (succ_index == unexplored) {
+                                    if (visited[j12]) {
+                                        // node visited but not added to composition, means it has no outgoing edges
+                                        // can skip this successor
+                                        continue;
+                                    }
+                                    succ_index = composition.add_node();
+                                    node_map[j12] = succ_index;
+                                    //std::cout << "Added new node " << succ_index << " from " << j1 << "," << j2 << std::endl; 
+                                }
+
+                                if (!visited[j12]) {
+                                    visited[j12] = true;
+                                    queue.push_back({j1, j2});
+                                    //std::cout << "Adding to queue " << succ_index << " from " << j1 << "," << j2 << std::endl; 
+                                }
+
+                                //std::cout << "Adding edges to " << pred_index << " from " << i1 << "," << i2 << std::endl; 
+                                Edge& edge = composition.add_edge(pred_index, succ_index, edge1.north, edge2.south);
                                 // this part can be omitted if one only needs to decide periodicity,
                                 // but is needed to actually to recover a periodic tiling
-                                edge->tiles.reserve(edge1.tiles.size() + edge2.tiles.size());
-                                edge->tiles.insert(std::end(edge->tiles), std::cbegin(edge1.tiles), std::cend(edge1.tiles));
-                                edge->tiles.insert(std::end(edge->tiles), std::cbegin(edge2.tiles), std::cend(edge2.tiles));
+                                edge.tiles.reserve(edge1.tiles.size() + edge2.tiles.size());
+                                edge.tiles.insert(std::end(edge.tiles), std::cbegin(edge1.tiles), std::cend(edge1.tiles));
+                                edge.tiles.insert(std::end(edge.tiles), std::cbegin(edge2.tiles), std::cend(edge2.tiles));
                             }
                         }
                     }
@@ -424,9 +530,15 @@ struct Transducer {
         return composition;
     }
 
+    void print_size() const {
+        Index num_edges = edges.size();
+        std::cout << "n = " << num_nodes << "; m = " << num_edges << std::endl;
+    }
+
     void print() const {
-        for (size_t i = 0; i < num_nodes; i++) {
-            for (const Edge& edge : succ_edges[i]) {
+        for (Index i = 0; i < num_nodes; i++) {
+            for (Index j = get_edge_begin(i); j < get_edge_end(i); j++) {
+                const Edge& edge = get_edge(j);
                 std::cout << "  " << i << " -> " << edge.target << " (" << (int)edge.north << "/" << (int)edge.south << ")" << std::endl;
             }
         }
@@ -450,6 +562,7 @@ TilesetResult test(const Tileset& tileset, int max_k, bool always_test = false, 
     if (verbosity >= 2) {
         std::cout << "Transducer for tileset without simplification" << std::endl;
         trans.print();
+        trans.print_debug();
     }
     trans.simplify();
     Transducer trans_k(num_colors);
@@ -460,14 +573,20 @@ TilesetResult test(const Tileset& tileset, int max_k, bool always_test = false, 
             std::cout << "Testing k = " << k << std::endl;
         }
         trans_k = trans_k.compose(trans);
+        if (verbosity >= 1) {
+            std::cout << "Size of composition: ";
+            trans_k.print_size();
+        }
         if (verbosity >= 2) {
             std::cout << "Transducer for k = " << k << " before simplification" << std::endl;
             trans_k.print();
+            trans_k.print_debug();
         }
         trans_k.simplify();
         if (verbosity >= 2) {
             std::cout << "Transducer for k = " << k << " after simplification" << std::endl;
             trans_k.print();
+            trans_k.print_debug();
         }
         if (trans_k.empty()) {
             // tileset is finite
